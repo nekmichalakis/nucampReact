57aa6e426443e2e2c59fd4c17fd7e9c56570c59d
Nektarios <nekmichalakis@gmail.com>
2022-03-17 15:18:58 -0400

    contact form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 class Header extends Component {
 
@@ -35,7 +35,7 @@ class Header extends Component {
 
                 <Navbar dark sticky="top" expand="md">
                     <div className="container">
-                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/logo.png" height="30" width="30" alt="NuCamp Logo" /></NavbarBrand>
+                        <NavbarBrand className="mr-auto"><Link to="/home"><img src="/assets/images/logo.png" height="30" width="30" alt="NuCamp Logo" /></Link></NavbarBrand>
                         <NavbarToggler onClick={this.toggleNav} />
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -13,22 +13,21 @@ function RenderCard({item}) {
     );
 }
 
-function Home(props) {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md m-1">
-                    <RenderCard item={props.campsite} />
-                </div>
-                <div className="col-md m-1">
-                    <RenderCard item={props.promotion} />
-                </div>
-                <div className="col-md m-1">
-                    <RenderCard item={props.partner} />
-                </div>
+const Home = (props) => (
+    
+    <div className="container">
+        <div className="row">
+            <div className="col-md m-1">
+                <RenderCard item={props.campsite} />
+            </div>
+            <div className="col-md m-1">
+                <RenderCard item={props.promotion} />
+            </div>
+            <div className="col-md m-1">
+                <RenderCard item={props.partner} />
             </div>
         </div>
-    );
-}
+    </div>
+)
 
 export default Home;
