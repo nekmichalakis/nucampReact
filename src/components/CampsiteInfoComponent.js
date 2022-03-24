8741698a43a59d66571bd013d7b1b0482a9e7bba
Nektarios <nekmichalakis@gmail.com>
2022-03-24 18:07:17 -0400

    fetch

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, 
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
@@ -91,7 +92,7 @@ function RenderCampsite({ campsite }) {
     return (
         <div className='col-md-5 m-1'>
             <Card>
-                <CardImg top src={image} alt={name} />
+                <CardImg top src={baseUrl + image} alt={name} />
                 <CardBody>
                     <CardText>{description}</CardText>
                 </CardBody>
